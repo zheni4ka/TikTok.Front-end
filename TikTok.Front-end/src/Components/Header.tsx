52222b6a5b5132f594c1655073b0e2571cd7a018
vo1ce <vo1ce@DESKTOP-ARNJTR2>
2024-07-03 20:56:02 +0300

    12345

--- TikTok.Front-end/src/Components/Header.tsx
+++ TikTok.Front-end/src/Components/Header.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import {Layout as LayoutAntd} from 'antd';
+import {Layout as LayoutAntd, Input, Button} from 'antd';
 import {TikTokOutlined} from '@ant-design/icons';
-import Link from "antd/es/typography/Link";
+import { Link } from "react-router-dom";
 
 const {Header : HeaderAntd} = LayoutAntd;
+const { Search } = Input;
 
-const Header : React.FC = () => (
-    <>
+const Header : React.FC = () => {
+
+    return (
         <HeaderAntd style={{ display: 'flex', alignItems: 'center' }}>
-            <div>
-                <span>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <span style={{display: 'flex'}}>
                     <TikTokOutlined />
+                    <Link to="/"><h1>TikTok</h1></Link>
                 </span>
+
+                <span>
+                    <Search placeholder="Search user here" allowClear style={{ width: 200, flexGrow: 1 }} />
+
+                </span>
+                
+                <Button>Login</Button>
+                <Button>Register</Button>
             </div>
         </HeaderAntd>
-    </>
-);
+    );
+};
 
 export default Header;
---
+++ TikTok.Front-end/src/Components/Layout.tsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+import React from "react";
+
+const Layout : React.FC = () => {
+    return (
+        <Header/>
+    )
+}
+
+export default Layout;
